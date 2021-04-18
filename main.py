fae1429839671ea00fb094e5567795be281fa116
TzviNoy <Tzvieln@gmail.com>
2021-04-18 20:41:47 +0300

    Add display function to examine final results

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import os
 import torch
+import matplotlib.pyplot as plt
 from training_pipeline import pipeline
 from neural_network import Net
+from data_handler import DataHandler
+from display import display
 
 if __name__ == "__main__":
 
-    path = os.path.join(os.getcwd(), "configuration.yaml")
+    mode = "train"
 
-    learning_rate = 9e-2
-    model = Net()
-    criterion = torch.nn.MSELoss(reduction='sum')
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    if mode == "train":
 
-    model = pipeline(path, model, criterion, optimizer, 100)
+        path = os.path.join(os.getcwd(), "configuration.yaml")
 
-    torch.save(model, "model.pt")
+        learning_rate = 9e-2
+        model = Net()
+        criterion = torch.nn.MSELoss(reduction='sum')
+        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+        model = pipeline(path, model, criterion, optimizer, 200)
+
+        torch.save(model, os.path.join(os.getcwd(), "model.pt"))
+
+    elif mode == "display":
+
+        config_path = "display_config.yaml"
+        model_path = os.path.join(os.getcwd(), "model.pt")
+
+        display(config_path, model_path)
